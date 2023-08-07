0bda74d318051bb9bed0927cdc43a97d0c75f2e9
Sumon <m.sumon@outlook.com>
2023-08-07 21:32:09 +0600

    category responsive done

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,3 +1,118 @@
+// import { Card, Col, Row } from "antd";
+// import Image from "next/image";
+// import {
+//   ArrowRightOutlined,
+//   StockOutlined,
+//   AppstoreAddOutlined,
+//   DollarOutlined,
+//   StarOutlined,
+// } from "@ant-design/icons";
+// import Link from "next/link";
+
+// const Features = ({ products }) => {
+//   const { Meta } = Card;
+//   return (
+//     <>
+//       <h2
+//         style={{
+//           textAlign: "center",
+//           fontSize: "35px",
+//           margin: "30px 0px",
+//           fontWeight: "bold",
+//           fontFamily: "cursive",
+//           color: "navy",
+//         }}
+//       >
+//         FIND CATEGORIES
+//       </h2>
+//       <Row
+//         gutter={{
+//           xs: 8,
+//           sm: 16,
+//           md: 24,
+//           lg: 32,
+//         }}
+//       >
+//         {products?.map((product) => (
+//           <Col
+//             style={{ marginBottom: "25px" }}
+//             key={product?._id}
+//             className="gutter-row"
+//             span={6}
+//           >
+//             <Card
+//               hoverable
+//               cover={
+//                 <Image
+//                   src={product?.image}
+//                   width={500}
+//                   height={200}
+//                   responsive
+//                   alt="product image"
+//                 />
+//               }
+//             >
+//               <Meta title={product?.productName} />
+//               <div
+//                 className="line"
+//                 style={{
+//                   height: "5px",
+//                   margin: "20px 0",
+//                   background: "navy",
+//                   width: "100%",
+//                 }}
+//               ></div>
+
+//               <p
+//                 style={{
+//                   display: "flex",
+//                   justifyContent: "space-between",
+//                   width: "100%",
+//                   color: "gray",
+//                   margin: "10px 0px",
+//                   fontSize: "12px",
+//                 }}
+//               >
+//                 <span>
+//                   <StockOutlined /> {product?.status}
+//                 </span>
+//                 <span>
+//                   <DollarOutlined /> {product?.price} $
+//                 </span>
+//                 <span>
+//                   <AppstoreAddOutlined /> {product?.category}
+//                 </span>
+//               </p>
+//               <p style={{ display: "block" }}>
+//                 <StarOutlined /> {product?.personalRating}
+//               </p>
+//               <Link href={`/product/${product?._id}`}>
+//                 <p
+//                   style={{
+//                     fontSize: "15px",
+//                     marginTop: "20px",
+//                     backgroundColor: "navy",
+//                     color: "white",
+//                     width: "100%",
+//                     padding: "2px 5px ",
+//                     fontWeight: "300",
+//                     letterSpacing: "3px",
+//                     textAlign: "center",
+//                   }}
+//                 >
+//                   Show Details <ArrowRightOutlined />
+//                 </p>
+//               </Link>
+//             </Card>
+//           </Col>
+//         ))}
+//       </Row>
+//     </>
+//   );
+// };
+
+// export default Features;
+
 import { Card, Col, Row } from "antd";
 import Image from "next/image";
 import {
@@ -38,7 +153,10 @@ const Features = ({ products }) => {
             style={{ marginBottom: "25px" }}
             key={product?._id}
             className="gutter-row"
-            span={6}
+            xs={24}
+            sm={12}
+            md={8}
+            lg={6}
           >
             <Card
               hoverable
@@ -94,7 +212,7 @@ const Features = ({ products }) => {
                     backgroundColor: "navy",
                     color: "white",
                     width: "100%",
-                    padding: "2px 5px ",
+                    padding: "2px 5px",
                     fontWeight: "300",
                     letterSpacing: "3px",
                     textAlign: "center",
